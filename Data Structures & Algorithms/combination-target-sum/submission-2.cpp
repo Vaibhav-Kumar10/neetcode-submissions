3734cc0bb4068af80d46cbaaa506d3f80f7bd142
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-09 00:01:18 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void f(int ind, int target, vector<int>& cur, vector<vector<int>>& ans, 
+            vector<int>& nums, int n) {
+        if(ind == n) {
+            if(target == 0) {
+                ans.push_back(cur);
+            }
+            return;
+        }
+        // Take current element and try itself again
+        if(nums[ind] <= target) {
+            cur.push_back(nums[ind]);
+            f(ind, target - nums[ind], cur, ans, nums, n);
+            cur.pop_back();
+        }
+
+        // Skip current element
+        f(ind + 1, target, cur, ans, nums, n);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ind = 0;
+        f(ind, target, cur, ans, nums, n);
+        return ans;
+    }
+};
