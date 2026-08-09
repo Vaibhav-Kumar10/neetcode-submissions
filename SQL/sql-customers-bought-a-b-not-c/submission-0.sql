a095ac198349bb9c385f021a48d09d6510dc5c13
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-09 23:29:47 +0530

    Add: sql-customers-bought-a-b-not-c - submission-0

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-0.sql
@@ -0,0 +1,18 @@
+SELECT c.customer_id, c.customer_name
+FROM customers AS c
+WHERE 
+c.customer_id IN (
+    SELECT o.customer_id FROM orders AS o
+    WHERE o.product_name = 'A'
+)
+AND
+c.customer_id IN (
+    SELECT o.customer_id FROM orders AS o
+    WHERE o.product_name = 'B'
+)
+AND
+c.customer_id NOT IN (
+    SELECT o.customer_id FROM orders AS o
+    WHERE o.product_name = 'C'
+)
+ORDER BY customer_name ASC;
