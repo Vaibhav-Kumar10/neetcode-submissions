d18ec25d2e1a3e88041b90bfbd0f3870ce055a3a
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-14 16:49:47 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cpp
@@ -0,0 +1,53 @@
+class Solution {
+   public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int one_step = 1, two_step = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int cur = one_step;
+            one_step = one_step + two_step;
+            two_step = cur;
+        }
+        return one_step;
+    }
+    /*
+    int climbStairs(int n) {
+        if(n <= 2) {
+            return n;
+        }
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int cur = 3; cur <= n; cur++) {
+            // 1 step
+            int one_step = dp[cur - 1];
+            // 2 step
+            int two_step = dp[cur - 2];
+            dp[cur] = one_step + two_step;
+        }
+        return dp[n];
+    }
+    */
+    /*
+    int f(int cur, int n, vector<int>& dp) {
+        if (cur >= n) {
+            return cur == n;
+        }
+        if (dp[cur] != -1) {
+            return dp[cur];
+        }
+        // 1 step
+        int one_step = f(cur + 1, n, dp);
+        // 2 step
+        int two_step = f(cur + 2, n, dp);
+        return dp[cur] = one_step + two_step;
+    }
+    int climbStairs(int n) {
+        int cur = 0;
+        vector<int> dp(n + 1, -1);
+        return f(cur, n, dp);
+    }
+    */
+};
