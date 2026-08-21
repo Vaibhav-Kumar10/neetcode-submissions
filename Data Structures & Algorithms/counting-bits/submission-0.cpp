4dac1a6da13b254b4f3aeb2183817941dac84e0a
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-21 21:51:15 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int num = 0; num <= n; num++) {
+            int one_cnt = 0, num_c = num;
+            while(num_c != 0) {
+                one_cnt += (num_c & 1);
+                num_c >>= 1;
+            }
+            ans.push_back(one_cnt);
+        }
+        return ans;
+    }
+};
