4351e942245e32608d93f51c30715cdf5b4ecb45
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2026-08-25 15:24:03 +0530

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+   public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ind = 0, prev = 0, n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev = ind - 1; prev >= -1; prev--) {
+                int take_cur = INT_MIN;
+                if ((prev == -1) || (nums[ind] > nums[prev])) {
+                    take_cur = 1 + dp[ind + 1][ind + 1];
+                }
+                int skip_cur = dp[ind + 1][prev + 1];
+                dp[ind][prev + 1] = max(take_cur, skip_cur);
+            }
+        }
+        return dp[0][0];
+    }
+    /*
+    int f(int ind, int prev, vector<int>& nums, int n, vector<vector<int>>& dp) {
+        if (ind == n) {
+            return 0;
+        }
+        if (dp[ind][prev + 1] != -1) {
+            return dp[ind][prev + 1];
+        }
+        int take_cur = INT_MIN;
+        if ((prev == -1) || (nums[ind] > nums[prev])) {
+            take_cur = 1 + f(ind + 1, ind, nums, n, dp);
+        }
+        int skip_cur = f(ind + 1, prev, nums, n, dp);
+        return dp[ind][prev + 1] = max(take_cur, skip_cur);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int ind = 0, prev = -1, n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return f(ind, prev, nums, n, dp);
+    }
+    */
+};
